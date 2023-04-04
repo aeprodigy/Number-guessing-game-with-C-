6c6092b0fcd9a211fb5b6468a3a0b540cf3a621d
Mike Mambwe <88389755+aeprodigy@users.noreply.github.com>
2023-04-04 13:02:35 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,23 @@
 #include <cstdlib>
 #include <time.h>
 
+//Header files above this line.
 using namespace std;
 
 int main()
 {
+   //This is the array of five numbers
    int arr[5] = {1,2,3,4,5};
-      // Initialize the srand seed.
+   // Initialize the srand seed.
     srand (time(NULL));
 
+   //variables below this line.
    int Random;
    int option;
    int exit;
 
 
+   //the loop below this line.
    for(int i = 0; i < 5; i++)
    {
        Random = arr[rand() % arr[i]];
